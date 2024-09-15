7bfe3e38c3138f8ca75a35daafb60f4a41d35fe3
Tyler Moore <tylerrmoore@gmail.com>
2024-09-15 07:32:54 -0700

    Working e-mail.

--- src/mail/mod.rs
+++ src/mail/mod.rs
@@ -24,6 +24,20 @@ pub fn get_service()->Result<MailService,Box::<dyn std::error::Error>>
     Ok(service)
 }
 
+fn get_transport(service: &MailService)->Result<SmtpTransport,Box::<dyn std::error::Error>>
+{
+    // Create TLS transport on port 465
+    let sender = SmtpTransport::relay(&service.smtp_url)?
+    .port(service.smtp_port)
+    .credentials(Credentials::new(
+        service.user.to_string(),
+        service.password.to_string(),
+    ))
+    .build();
+
+    Ok(sender)
+}
+
 pub fn send_mail(
     service: &MailService,
     recipient: &str,
@@ -39,15 +53,30 @@ pub fn send_mail(
         .subject(subject)
         .body(body)?;
 
-    // Create TLS transport on port 465
-    let sender = SmtpTransport::relay(&service.smtp_url)?
-        .port(service.smtp_port)
-        .credentials(Credentials::new(
-            service.user.to_string(),
-            service.password.to_string(),
-        ))
-        .build();
+    let sender = get_transport(service)?;
 
     // Send the email via remote relay
     Ok(sender.send(&email)?)
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_email_connectivity() {
+        let service = get_service().expect("Couldn't get service.");
+        let sender = get_transport(&service).expect("Couldnt get transport.");
+        sender.test_connection().expect("Connection test failed.");
+    }
+
+    #[test]
+    fn test_self_email_send() {
+        let service = get_service().expect("Couldn't get service.");
+        send_mail(
+            &service,
+            &service.from_address,
+            "Block Divider E-mail Test".to_string(),
+            "This is a test of programmatic e-mailing.".to_string()
+        ).expect("Couldn't send e-mail.");
+    }
 }
