70f4f2808a3580a53a3c732655aaa9a69c954f9b
Tyler Moore <tylerrmoore@gmail.com>
2024-09-17 07:12:18 -0700

    Trying to provide multiple different response types

--- core/src/server/commons.rs
+++ core/src/server/commons.rs
@@ -1,14 +1,15 @@
 use std::{
+    error::Error,
     future::Future,
     net::{IpAddr, SocketAddr},
     pin::Pin,
 };
 
-use http_body_util::Full;
+use http_body_util::{combinators::BoxBody, Full};
 use hyper::{
-    body::{Bytes, Incoming},
+    body::{Body, Bytes, Incoming},
     server::conn::http1,
-    service::Service,
+    service::{HttpService, Service},
     Request, Response,
 };
 use hyper_util::rt::{TokioIo, TokioTimer};
@@ -18,27 +19,16 @@ use tokio::net::TcpListener;
 pub(crate) async fn spawn_server<S>(
     ip: IpAddr,
     port: u16,
-    service: &S,
+    service: S,
 ) -> Result<(), Box<dyn std::error::Error + Send + Sync>>
 where
-    S: 'static
-        + Clone
-        + Send
-        + Service<
-            Request<Incoming>,
-            Response = Response<Full<Bytes>>,
-            Error = Box<dyn std::error::Error + Send + Sync>,
-            Future = Pin<
-                Box<
-                    dyn Future<
-                            Output = Result<
-                                Response<Full<Bytes>>,
-                                Box<dyn std::error::Error + Send + Sync>,
-                            >,
-                        > + Send,
-                >,
-            >,
-        >,
+    S: 'static + Clone + Send + HttpService<Incoming>,
+    S::Error: Into<Box<dyn std::error::Error + Send + Sync>>,
+    S::ResBody: 'static,
+    <S::ResBody as Body>::Error: Into<Box<dyn std::error::Error + Send + Sync>>,
+    <S as HttpService<hyper::body::Incoming>>::ResBody: Send,
+    <S as HttpService<hyper::body::Incoming>>::Future: Send,
+    <<S as HttpService<hyper::body::Incoming>>::ResBody as hyper::body::Body>::Data: Send,
 {
     let socket = SocketAddr::new(ip, port);
     let listener = TcpListener::bind(socket).await?;
--- core/src/server/handler.rs
+++ core/src/server/handler.rs
@@ -1,19 +1,21 @@
 use std::{future::Future, pin::Pin};
 
-use http_body_util::{BodyExt, Either, Empty, Full};
+use http_body_util::{combinators::BoxBody, BodyExt, Either, Empty, Full};
 use hyper::{
-    body::{Body, Bytes, Incoming},
+    body::{Body, Bytes, Frame, Incoming},
     service::Service,
-    Request, Response,
+    Method, Request, Response,
 };
 use serde::{Deserialize, Serialize};
 
 use crate::data::block_division::BlockDivisionInput;
 
+use futures_util::TryStreamExt;
+
+type HandlerResponse = Response<BoxBody<Bytes, std::io::Error>>;
+
 #[derive(Clone)]
 pub struct PostHandler {}
-
-type HandlerResponse = Response<Full<Bytes>>;
 
 impl Service<Request<Incoming>> for PostHandler {
     type Response = HandlerResponse;
@@ -35,14 +37,15 @@ impl PostHandler {
         request: Request<Incoming>,
     ) -> Result<HandlerResponse, Box<dyn std::error::Error + Send + Sync>> {
         let method = request.method().clone();
-        let path = request.uri().path().to_string();
+        let path = request.uri().path();
         let headers = request.headers().clone();
 
         println!("Path: {}", path);
 
-        match path.as_str() {
-            "bdd" => Self::bdd(request).await,
-            "echo" => Self::echo(request).await,
+        match (method, path) {
+            (Method::POST, "bdd") => Self::bdd(request).await,
+            (Method::POST, "echo") => Self::echo(request).await,
+            (Method::GET, path) => Self::send_file(path.to_string()).await,
             _ => {
                 return Ok(Self::not_found());
             }
@@ -54,6 +57,45 @@ impl PostHandler {
             .status(hyper::StatusCode::NOT_FOUND)
             .body(Full::new(Bytes::from("Resource not found.")))
             .expect("Should produce response.")
+    }
+
+    fn bad_request() -> Response<Full<Bytes>> {
+        Response::builder()
+            .status(hyper::StatusCode::BAD_REQUEST)
+            .body(Full::new(Bytes::from("Malformed request.")))
+            .expect("Should produce response.")
+    }
+
+    async fn send_file(
+        path: String,
+    ) -> Result<HandlerResponse, Box<dyn std::error::Error + Send + Sync>> {
+        if path.contains("..") {
+            //Reject attempts to access parent directories
+            return Ok(Self::bad_request());
+        } else {
+            match tokio::fs::File::open(path).await {
+                Ok(file) => {
+                    let reader_stream = tokio_util::io::ReaderStream::new(file);
+
+                    // Convert to http_body_util::BoxBody
+                    let stream_body =
+                        http_body_util::StreamBody::new(reader_stream.map_ok(Frame::data));
+                    let boxed_body = stream_body.boxed();
+
+                    // Send response
+                    let response = Response::builder()
+                        .status(hyper::StatusCode::OK)
+                        .body(boxed_body)
+                        .unwrap();
+
+                    Ok(response)
+                }
+                Err(e) => {
+                    eprintln!("{:?}", e);
+                    return Ok(Self::not_found());
+                }
+            }
+        }
     }
 
     async fn get_request_body_as_string(
@@ -69,7 +111,7 @@ impl PostHandler {
         request: Request<Incoming>,
     ) -> Result<HandlerResponse, Box<dyn std::error::Error + Send + Sync>> {
         let as_string = Self::get_request_body_as_string(request).await?;
-        Ok(Response::new(Full::new(Bytes::from(as_string))))
+        Ok(Response::new(BoxBody::new(Bytes::from(as_string))))
     }
 
     async fn bdd(
@@ -81,7 +123,9 @@ impl PostHandler {
             Ok(data) => data,
             Err(_e) => {
                 eprintln!("Could not parse package. {:?}", &as_string);
-                return Ok(Response::new(Full::new(Bytes::from("Invalid JSON"))));
+                return Ok(Response::new(BoxBody::new(Full::new(Bytes::from(
+                    "Invalid JSON",
+                )))));
             }
         };
 
