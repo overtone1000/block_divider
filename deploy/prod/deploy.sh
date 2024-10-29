75abe479ca3cbb5a05a3cf77dcde9ea4b53ba785
Tyler Moore <TylerRMoore@gmail.com>
2024-10-28 17:03:23 -0700

    Version 0.1.0
    Signed-off-by: Tyler Moore <TylerRMoore@gmail.com>

--- deploy/prod/deploy.sh
+++ deploy/prod/deploy.sh
@@ -13,5 +13,8 @@ TMP_FOLDER=/tmp/stack
 ssh -l $REMOTE_USER $REMOTE_IP "mkdir -p $TMP_FOLDER"
 scp -r $ENV_F $REMOTE_USER@$REMOTE_IP:$TMP_FOLDER/.env
 scp -r $STACK $REMOTE_USER@$REMOTE_IP:$TMP_FOLDER/stack.yml
+ssh -l $REMOTE_USER $REMOTE_IP "podman pull docker.io/overtone1000/block_divider:prod"
+ssh -l $REMOTE_USER $REMOTE_IP "podman compose --env-file $TMP_FOLDER/.env -f $TMP_FOLDER/stack.yml down"
+ssh -l $REMOTE_USER $REMOTE_IP "podman container prune -f"
 ssh -l $REMOTE_USER $REMOTE_IP "podman compose --env-file $TMP_FOLDER/.env -f $TMP_FOLDER/stack.yml up --detach"
 ssh -l $REMOTE_USER $REMOTE_IP "rm -rdf $TMP_FOLDER"
