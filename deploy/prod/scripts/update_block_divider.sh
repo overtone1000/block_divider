65f5b2621908b73b00ed2af1111f6429337aeb27
Tyler Moore <TylerRMoore@gmail.com>
2024-11-02 10:23:23 -0700

    Updates breaking dns?

--- deploy/prod/scripts/update_block_divider.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-source ./server.sh
-
-ssh -l $REMOTE_USER $REMOTE_IP "podman pull docker.io/overtone1000/block_divider:prod"
-ssh -l $REMOTE_USER $REMOTE_IP "podman restart stack_core_1"
