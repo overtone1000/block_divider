a88be53003f58f352f92ba7688ff38d978ec248a
Tyler Moore <TylerRMoore@gmail.com>
2024-11-12 21:03:16 -0800

    Version 0.3.0
    Signed-off-by: Tyler Moore <TylerRMoore@gmail.com>

---
+++ deploy/prod/scripts/update_block_divider_core.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+source ./server.sh
+
+STACK="../docker/stack.yml"
+ENV_F="../.env"
+
+TMP_FOLDER=/tmp/stack
+
+ssh -l $REMOTE_USER $REMOTE_IP "mkdir -p $TMP_FOLDER"
+scp -r $ENV_F $REMOTE_USER@$REMOTE_IP:$TMP_FOLDER/.env
+scp -r $STACK $REMOTE_USER@$REMOTE_IP:$TMP_FOLDER/stack.yml
+ssh -l $REMOTE_USER $REMOTE_IP "podman pull docker.io/overtone1000/block_divider:prod"
+ssh -l $REMOTE_USER $REMOTE_IP "podman compose --env-file $TMP_FOLDER/.env -f $TMP_FOLDER/stack.yml stop stack_block_divider_core_1"
+ssh -l $REMOTE_USER $REMOTE_IP "podman container prune -f"
+ssh -l $REMOTE_USER $REMOTE_IP "podman compose --env-file $TMP_FOLDER/.env -f $TMP_FOLDER/stack.yml start stack_block_divider_core_1"
+ssh -l $REMOTE_USER $REMOTE_IP "rm -rdf $TMP_FOLDER"
